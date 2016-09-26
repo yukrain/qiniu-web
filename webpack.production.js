e8a5e74a71f6cf7adf5b96c616a5f6020284ebdf
yukrain <yukrain@163.com>
2016-09-26 16:48:21 +0800

    ingore assets

--- webpack.production.js
+++ webpack.production.js
@@ -8,25 +8,28 @@ config.output.filename = '[name].js?[hash]';
 config.output.chunkFilename = '[id].bundle.js?[hash]';
 config.plugins = [
     //new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
-
-
     new webpack.ProvidePlugin({
         "React": "react",
         "ReactDOM": "react-dom",
     }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({
-        'process.env.NODE_ENV': '"production"'
-    }),
-
 
     new webpack.optimize.UglifyJsPlugin({
         compress: {
             warnings: false
         }
     }),
-    new webpack.NoErrorsPlugin()
 
+    new webpack.DefinePlugin({
+        "process.env": {
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+        },
+    }),
+
+
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+
+    new webpack.NoErrorsPlugin()
 ]
 
 
